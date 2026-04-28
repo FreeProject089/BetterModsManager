4c673e2ee4da7f2089f528f94dbd988a7d8f8e92
FreeProject089 <enrique089alarcon@gmail.com>
2026-04-29 00:22:46 +0200

    minor change gitgnore

--- scratch/find_missing_keys.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('frontend/index.html', 'utf8');
-const fr = JSON.parse(fs.readFileSync('frontend/Lang/fr.json', 'utf8'));
-const en = JSON.parse(fs.readFileSync('frontend/Lang/en.json', 'utf8'));
-const keysInHtml = [...html.matchAll(/data-i18n=\"(docs\.gallery\.btn\.[^\"]+)\"/g)].map(m => m[1]);
-const missingFr = keysInHtml.filter(k => fr[k] === undefined);
-const missingEn = keysInHtml.filter(k => en[k] === undefined);
-console.log('Missing in fr.json:', missingFr);
-console.log('Missing in en.json:', missingEn);
